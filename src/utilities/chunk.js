20dadbe44e02ff10ce1d499fb9709851f79fc2bf
Bruno Wacemberg <brunowacemberg@gmail.com>
2021-01-25 22:28:01 -0300

    Change chunk script

--- src/utilities/chunk.js
+++ src/utilities/chunk.js
@@ -1,19 +1,6 @@
-"use strict";
+const chunk = (arr, size) =>
+Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+arr.slice(i * size, i * size + size)
+);
 
-(function () {
-    
-    const chunk = (arr, size) =>
-        Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
-        arr.slice(i * size, i * size + size)
-    );
-
-  if (typeof exports !== "undefined") {
-    if (typeof module !== "undefined" && module.exports) {
-      exports = module.exports = chunk;
-    }
-    exports.chunk = chunk;
-  } else {
-    this.chunk = chunk;
-  }
-  
-}.call(this));
+export default chunk;
