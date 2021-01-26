cda1fd7dd103f17fd8ee5fa13840b03c1d9a290f
Bruno Wacemberg <brunowacemberg@gmail.com>
2021-01-25 21:19:42 -0300

    Add current utilities

---
+++ utilities.js
@@ -0,0 +1,11 @@
+import chunk from './utilities/chunk';
+import debounce from './utilities/debounce';
+import normalizeSearchString from './utilities/normalizeSearchString';
+import scrollTo from './utilities/scrollTo';
+
+export {
+    chunk,
+    debounce,
+    normalizeSearchString,
+    scrollTo
+}
